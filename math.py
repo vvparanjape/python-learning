7913d6ce6c0ccf0b4cadf17c354cfddf0d2c8ab0
Vishwanath Paranjape <vparanjape@yahoo.com>
2021-04-13 00:30:02 +0530

    Added file math.py

---
+++ math.py
@@ -0,0 +1,6 @@
+from math import *
+temp_fah = input("Enter temperature in fahernheit: \n")
+print("You Entered " + str(temp_fah))
+temp_cel = ((float(temp_fah) - 32) * 5/9)
+print("Temperature in celcius is: " + str(temp_cel))
+
